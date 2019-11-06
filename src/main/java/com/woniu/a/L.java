a1506a625228ffec88f45771ac2ec5258e473414
lin <13087699146@163.com>
2019-11-06 20:37:06 +0800

    添加L类

---
+++ src/main/java/com/woniu/a/L.java
@@ -0,0 +1,7 @@
+package com.woniu.a;
+
+public class L {
+	public static void main(String[] args) {
+		System.out.println("网好卡");
+	}
+}
--- src/main/java/com/woniuxy/zhangjiatian/User.java
+++
@@ -1,7 +0,0 @@
-package com.woniuxy.zhangjiatian;
-
-public class User {
-	public static void main(String[] args) {
-		System.out.println("User.main()");
-	}
-}
