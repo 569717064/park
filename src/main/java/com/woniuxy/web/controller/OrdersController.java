138a71209691f39dcb8e1eb3d2190a22614a57d1
zhang <1534592131@qq.com>
2019-11-12 16:10:14 +0800

    update ordersMapper And OrdersController

--- src/main/java/com/woniuxy/web/controller/OrdersController.java
+++ src/main/java/com/woniuxy/web/controller/OrdersController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,22 +22,23 @@ public class OrdersController {
 	private IOrdersService ordersServiceImpl;
 	
 	@PostMapping
-	public void save() {
-		
+	public void save(@RequestBody Orders orders) {
+		ordersServiceImpl.save(orders);
 	}
 	
 	@DeleteMapping
-	public void delete() {
-		
+	public void delete(Integer oid) {
+		ordersServiceImpl.delete(oid);
 	}
 	
 	@PutMapping
-	public void update() {
-		
+	public void update(@RequestBody Orders orders) {
+		ordersServiceImpl.update(orders);
 	}
 	
 	@GetMapping
 	public List<Orders> findAll(){
-		return null;
+		List<Orders> ordersList = ordersServiceImpl.findAll();
+		return ordersList;
 	}
 }
