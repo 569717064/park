210337f278f9ac3bf601d19dd5ccb9b577275f6a
tian <1339081044@qq.com>
2019-11-19 19:04:11 +0800

    添加了后台UserController OrdersController类

--- src/main/java/com/woniuxy/web/controller/OrdersController.java
+++ src/main/java/com/woniuxy/web/controller/OrdersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,8 +27,8 @@ public class OrdersController {
 		ordersServiceImpl.save(orders);
 	}
 	
-	@DeleteMapping
-	public void delete(Integer oid) {
+	@DeleteMapping("{oid}")
+	public void delete(@PathVariable Integer oid) {
 		ordersServiceImpl.delete(oid);
 	}
 	
